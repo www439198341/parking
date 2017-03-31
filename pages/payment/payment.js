e45b0d827b2dd02151f29b13decbaa797e76508f
www439198341 <www439198341@gmail.com>
2017-03-31 08:54:45 +0800

    added

---
+++ pages/payment/payment.js
@@ -0,0 +1,24 @@
+// pages/payment/payment.js
+Page({
+  data:{
+     carNumber:'苏AW526Z',
+     parkTime:'1:25:38',
+     parkLocation:'万达广场',
+     fee:'5.5'
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
