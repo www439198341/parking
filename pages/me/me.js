2cb308ffd6275316761e36231210453cade2cd1b
www439198341 <www439198341@gmail.com>
2017-04-29 01:32:48 +0800

    优化me界面

--- pages/me/me.js
+++ pages/me/me.js
@@ -3,91 +3,120 @@
 var app = getApp()
 Page({
   data: {
-    motto1: '车牌管理',
-    motto2: '停车记录',
-    motto3: '停车券',
-    motto4: '帮助',
     userInfo: {},
-    carNumber:null
+    carNumber: null,
+    userListInfo: [
+      {
+        icon: '../../image/iconfont-dingdan.png',
+        text: '我的车牌号',
+        isunread: false,
+        unreadNum: 2
+      },
+      {
+        icon: '../../image/iconfont-card.png',
+        text: '优惠券',
+        isunread: true,
+        unreadNum: 2
+      },
+      // {
+      //   icon: '../../image/iconfont-icontuan.png',
+      //   text: '我的拼团',
+      //   isunread: true,
+      //   unreadNum: 1
+      // },
+      {
+        icon: '../../image/iconfont-shouhuodizhi.png',
+        text: '停车记录'
+      },
+      // {
+      //   icon: '../../image/iconfont-kefu.png',
+      //   text: '联系客服'
+      // },
+      {
+        icon: '../../image/iconfont-help.png',
+        text: '常见问题'
+      }
+    ]
   },
   onShow: function () {
-    
+
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
-    that.getDefaultCarNo(function(res){
-      that.setData({carNumber:res[0].Address})
+    that.getDefaultCarNo(function (res) {
+      that.setData({ carNumber: res[0].Address })
     })
   },
-  carNoMgr : function(){
+  navi0: function () {
+    console.log("first clidk")
     wx.navigateTo({
       url: '../carNoMgr/carNoMgr',
-      success: function(res){
+      success: function (res) {
         // success
       },
-      fail: function(res) {
+      fail: function (res) {
         // fail
       },
-      complete: function(res) {
+      complete: function (res) {
         // complete
       }
     })
   },
-  logs:function(){
+  navi3: function () {
     wx.navigateTo({
       url: '../logs/logs',
-      success: function(res){
+      success: function (res) {
         // success
       },
-      fail: function(res) {
+      fail: function (res) {
         // fail
       },
-      complete: function(res) {
+      complete: function (res) {
         // complete
       }
     })
   },
-  onsale:function(){
+  navi1: function () {
     wx.navigateTo({
       url: '../onsale/onsale',
-      success: function(res){
+      success: function (res) {
         // success
       },
-      fail: function(res) {
+      fail: function (res) {
         // fail
       },
-      complete: function(res) {
+      complete: function (res) {
         // complete
       }
     })
   },
-  help:function(){
+  navi5: function () {
     wx.navigateTo({
       url: '../help/help',
-      success: function(res){
+      success: function (res) {
         // success
       },
-      fail: function(res) {
+      fail: function (res) {
         // fail
       },
-      complete: function(res) {
+      complete: function (res) {
         // complete
       }
     })
   },
   // 查询默认车牌号
-  getDefaultCarNo : function(cb){
+  getDefaultCarNo: function (cb) {
     wx.request({
       url: 'http://localhost:8080/TingChe/servlet/GetCarNo',
       data: {
-        openid:app.globalData.openid
+        openid: app.globalData.openid
       },
-      success: function(res){
+      success: function (res) {
         return typeof cb == "function" && cb(res.data)
       }
     })
